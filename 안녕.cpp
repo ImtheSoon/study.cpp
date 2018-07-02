dafac66b8ebf736149e94e8829aa393930b6c9f3
ImtheSoon <40754133+ImtheSoon@users.noreply.github.com>
2018-07-02 23:57:47 +0900

    Rename 앙냥.cpp to 안녕.cpp

---
+++ 안녕.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	
+	cout<< "¾È³ç C++"<< endl;//cout Ãâ·Â cin ÀÔ·Â 
+}
--- 앙냥.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-using namespace std;
-int main(){
-	
-	cout<< "안녕 C++"<< endl;//cout 출력 cin 입력 
-}
